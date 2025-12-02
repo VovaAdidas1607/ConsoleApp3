3b147d88dff398e58887743b8a1c6d29095feef8
VovaAdidas1607 <pakdaniil149@gmail.com>
2025-12-02 19:16:00 +0700

    vvod_studentov

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -25,23 +25,37 @@ namespace ConsoleApp3
                 int input = Convert.ToInt32(Console.ReadLine());
                 switch (input)
                 {
+                    //Создание студента
                     case 1:
                         string name;
                         string lastname;
                         double mark;
                         int id;
+
+                        //Обработка вводимого студента
                         Console.WriteLine("Имя");
                         name = Console.ReadLine();
+                        if (Validator.validation(name, true)) return;
                         Console.WriteLine("Фамилия");
                         lastname = Console.ReadLine();
-                        id = Convert.ToInt32(Console.ReadLine());
+                        if (Validator.validation(lastname, true)) return;
+                        string _id = Console.ReadLine();
+                        if (!Validator.validation(_id, true)) return;
+                        id = Convert.ToInt32(_id);
                         mark = Convert.ToDouble(Console.ReadLine());
                         Student student = new Student(name, lastname, id, mark);
                         students.Add(student);
                         break;
+
+
+
+                        //Вывод списка студентов
                     case 2:
                         PrintAllStudents(students);
                         break;
+
+
+                        //Поиск студента в списке
                     case 3:
                         Console.WriteLine("Введите фамилию");
                         if (SearchByLastName(students)){
@@ -52,6 +66,8 @@ namespace ConsoleApp3
                             Console.WriteLine("Студент нету в списке");
                         }
                             break;
+
+                        //Выход из програмы
                     case 6:
                         Console.WriteLine("Выхожу из программы");
                         exit = true;
@@ -59,7 +75,7 @@ namespace ConsoleApp3
                 }
             }
         }
-        static void PrintAllStudents(List<Student> students)
+        static void PrintAllStudents(List<Student> students) //Функция вывода списка студентов
         {
             foreach (Student student in students)
             {
---
+++ ConsoleApp3/Validator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Validator
+    {
+        public static bool validation(string text, bool isDigit)
+        {
+            string temp = text;
+
+            //Проверка на пустоту
+            if (String.IsNullOrEmpty(text))
+            {
+                return false;
+            }
+
+            if (isDigit)
+            {
+
+                //Проверка на цифры
+                if (int.TryParse(temp, out int value))
+                {
+                    Console.WriteLine("Заполните поле буквами");
+                    return true;
+                }
+            }
+            else
+            {
+
+                //Проверка на буквы
+                if (!temp.All(char.IsLetter))
+                {
+                    Console.WriteLine("Заполните поле цифрами");
+                    return false;
+
+                }
+            }
+            return true;
+        }
+    }
+}
